3b4f1fa92f244fb6ff2b3a171f0d4d4032ae44d6
rtlessly <tlessly@hotmail.com>
2017-03-07 14:55:52 -0600

    Initial Commit

---
+++ IRRemoteReceiver.cpp
@@ -0,0 +1,44 @@
+#define DEBUG 0
+
+#include "IRRemoteReceiver.h"
+
+
+static DebugHelper Debug("IRRemoteReceiver");
+
+
+int IRRemoteReceiver::COMMAND_RECV_EVENT = EventSource::GenerateEventID(); 
+int IRRemoteReceiver::COMMAND_REPT_EVENT = EventSource::GenerateEventID(); 
+
+
+void IRRemoteReceiver::Initialize()
+{
+    Debug.Log("Initialize");
+    enableIRIn();
+}
+
+
+void IRRemoteReceiver::Poll()
+{
+    decode_results results;
+  
+    // check to see if the receiver has received a command
+    if (decode(&results))
+    {
+        int eventID = COMMAND_REPT_EVENT;
+    
+        if (results.value != IR_REPEAT)
+        {
+          eventID = COMMAND_RECV_EVENT;
+          _lastValue = results.value;      
+        }
+        
+        Event event(eventID, _lastValue);
+        
+        // Queue an IR Remote event
+        DispatchEvent(&event);
+        
+        // tell the library we're ready to listen for another command
+        resume();
+    }
+}
+
---
+++ IRRemoteReceiver.h
@@ -0,0 +1,67 @@
+#ifndef _IRRemoteReceiver_H_
+#define _IRRemoteReceiver_H_
+
+#include <Arduino.h>
+#include <Debug.h>
+#include <EventSource.h>
+#include <IRremote2.h>
+
+
+class IRRemoteReceiver : public IRrecv, public EventSource
+{
+    public: static int COMMAND_RECV_EVENT; 
+    public: static int COMMAND_REPT_EVENT; 
+
+    //****************************************************************************
+    // Constructors
+    //****************************************************************************
+    public: IRRemoteReceiver(int pin=12, int ledPin=13)  : IRrecv(pin, ledPin) { };
+
+    //****************************************************************************
+    // Public methods
+    //****************************************************************************
+    public: void Initialize();
+
+    public: virtual void Poll();
+
+    //****************************************************************************
+    // Internal State & Behavior
+    //****************************************************************************
+    private: unsigned long _lastValue;
+};
+
+
+#define IR_NONE 0
+
+#define IR_CH_MINUS 0xFFA25D
+#define IR_CH 0xFF629D
+#define IR_CH_PLUS 0xFFE21D
+
+#define IR_PREV 0xFF22DD
+#define IR_NEXT 0xFF02FD
+#define IR_PLAY 0xFFC23D
+
+#define IR_VOL_MINUS 0xFFE01F
+#define IR_VOL_PLUS 0xFFA857
+#define IR_VOL_EQ 0xFF906F
+
+#define IR_0 0xFF6897
+#define IR_100_PLUS 0xFF9867
+#define IR_200_PLUS 0xFFB04F
+
+#define IR_1 0xFF30CF
+#define IR_2 0xFF18E7
+#define IR_3 0xFF7A85
+
+#define IR_4 0xFF10EF
+#define IR_5 0xFF38C7
+#define IR_6 0xFF5AA5
+
+#define IR_7 0xFF42BD
+#define IR_8 0xFF4AB5
+#define IR_9 0xFF52AD
+
+#define IR_REPEAT 0xFFFFFFFF
+
+#endif
+
